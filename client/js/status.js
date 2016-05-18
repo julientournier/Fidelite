6749083a62ac0bf28735394f49ffc1d67464814b
julientournier <julien_tournier@hotmail.com>
2016-05-18 17:39:11 +0200

    Update status.js

--- client/js/status.js
+++ client/js/status.js
@@ -17,7 +17,7 @@ angular.module('nibs.status', [])
                     title: 'Congratulations, ' + $rootScope.user.firstname + '!',
                     template:
                         '<div style="text-align: center" class="status' + data.newStatus + '">' +
-                        '<p>You have achieved a new status!</p>' +
+                        '<p>vous avez atteint un nouveau statut!</p>' +
                         '<img src="css/img/icon-status' + data.newStatus + '.svg" height="120"/>' +
                         '<h3 class="status' + data.newStatus + '">' + STATUS_LABELS[data.newStatus - 1] + ' Member</h3>' +
                         '<h4 class="status' + data.newStatus + '">' + data.newBalance + ' Nibs</h4>',
